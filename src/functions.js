9a21371ab92831a8029bf250fae2e9f649bdcf2b
Mateo Astudillo <ast.mateo@gmail.com>
2023-11-22 10:23:31 -0300

    :tada:first commit

---
+++ src/functions.js
@@ -0,0 +1,62 @@
+const getDataByName = async(cocktail) => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`;
+    console.log(url)
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("Error server");
+    }
+    const data = await response.json();
+    return data["drinks"];
+}
+
+const getData = async (id) => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("Error server");
+    }
+    const data = await response.json();
+    return data["drinks"][0];
+}
+
+const createCard = (id, name, sourceImage, instruction) => {
+    // ELEMENTS
+    const $cardConteiner = document.createElement("div");
+    const $drinkName = document.createElement("h3");
+    const $drinkImage = document.createElement("img");
+    const $drinkInstruction = document.createElement("p");
+    const $drinkInstructionContainer = document.createElement("div");
+    const $drinkButton = document.createElement("button");
+
+    // TITLE
+    $drinkName.textContent = name;
+    $drinkName.setAttribute("class", "drinkTitle");
+
+    // IMAGE
+    $drinkImage.setAttribute("src", sourceImage);
+    $drinkImage.setAttribute("class", "drinkImage");
+
+    // INSTRUCTION
+    $drinkInstruction.textContent = instruction;
+    $drinkInstruction.setAttribute("class", "drinkInstruction")
+
+    $drinkInstructionContainer.setAttribute("class", "drinkInstructionContainer");
+    $drinkInstructionContainer.appendChild($drinkInstruction)
+
+    // BUTTON
+    $drinkButton.textContent = "dar reseña".toUpperCase();
+    $drinkButton.setAttribute("class", "drinkButton")
+    $drinkButton.addEventListener("click", () => {
+        mostrarFormulario(id);
+    });
+
+    // CARD
+    $cardConteiner.setAttribute("class", "drink")
+    $cardConteiner.setAttribute("id", id)
+    $cardConteiner.appendChild($drinkName);
+    $cardConteiner.appendChild($drinkImage);
+    $cardConteiner.appendChild($drinkInstructionContainer);
+    $cardConteiner.appendChild($drinkButton);
+
+    return $cardConteiner
+}
---
+++ src/script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const drinksWithInstructionsES = [11028, 17227, 11013, 16202, 17833, 14610, 15194, 12710, 15941, 11010];
+    const main = document.getElementById("main");
+
+    drinksWithInstructionsES.forEach(async (id) => {
+        const drink = await getData(id);
+        const card = createCard(drink["idDrink"], drink["strDrink"], drink["strDrinkThumb"], drink["strInstructionsES"]);
+        main.appendChild(card);
+    });
+})
+
+document.getElementById("searchButton").addEventListener("click", async() => {
+    const searchedConteiner = document.getElementById("searchedContainer");
+    const searchInput = document.getElementById("searchInput");
+
+    while (searchedConteiner.firstChild) {
+        searchedConteiner.removeChild(searchedConteiner.firstChild);
+    }
+
+    let cocktail = searchInput.value;
+    if (!/^[A-Za-z ]+$/.test(cocktail)) {
+        searchInput.setAttribute("id", "invalidText");
+        setTimeout(() => {
+            searchInput.setAttribute("id", "searchInput");
+            searchInput.value = "";
+        }, 2000);
+        return
+    }
+
+    const drinks = await getDataByName(cocktail);
+    console.log(drinks);
+    if (drinks == null) {
+        setTimeout(() => {
+            document.getElementById("messageNotFound").style = "display: block;";
+
+            searchInput.value = "";
+        }, 2000);
+        document.getElementById("notFound").style = "display: none;";
+        return
+    }
+
+    drinks.forEach((drink) => {
+        let strInstruction = drink["strInstructionsES"];
+        console.log(strInstruction);
+        if (strInstruction == null) {
+            strInstruction = drink["strInstructions"]
+        }
+        const card = createCard(drink["idDrink"], drink["strDrink"], drink["strDrinkThumb"], strInstruction);
+        searchedConteiner.appendChild(card);
+    })
+    searchInput.value = "";
+});
+
